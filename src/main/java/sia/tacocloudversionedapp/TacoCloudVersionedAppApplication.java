d42b2c0e11c7884dc696fc54c31cb7b52037dbc9
Livia Manescu <livia.manescu@endava.com>
2023-12-27 19:22:15 +0200

    first commit

---
+++ src/main/java/sia/tacocloudversionedapp/TacoCloudVersionedAppApplication.java
@@ -0,0 +1,13 @@
+package sia.tacocloudversionedapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudVersionedAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudVersionedAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sia/tacocloudversionedapp/TacoCloudVersionedAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sia.tacocloudversionedapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudVersionedAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
